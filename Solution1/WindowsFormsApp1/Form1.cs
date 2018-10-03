6c4e4addb72747362fe089e232a6fda9430d83ae
YasinthaNimahSafitri <43593350+YasinthaNimahSafitri@users.noreply.github.com>
2018-10-04 00:15:45 +0700

    Add files via upload

---
+++ Solution1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClassLibrary1;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        Terbilang terbilang = new Terbilang();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var i = Convert.ToInt32(textBox1.Text);
+
+            richTextBox1.Text = (Convert.ToString(terbilang.TerbilangIndonesia(i)));
+
+        }
+    }
+}
